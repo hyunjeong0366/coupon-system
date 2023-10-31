6d27da7d7b9cd160a3b944d4fa0503e7f95f8b11
hyunjeong.j <hyunjeong.jeong@hanwha.com>
2023-10-31 17:57:19 +0900

    kafka consumer

--- api/src/main/java/com/example/api/producer/CouponCreateProducer.java
+++ api/src/main/java/com/example/api/producer/CouponCreateProducer.java
@@ -14,5 +14,4 @@ public class CouponCreateProducer {
     public void create(Long userId) {
         kafkaTemplate.send("coupon_create", userId);
     }
-
 }
--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,22 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
         Long count = couponCountRepository.increment();
+
+        if (apply != 1) {
+            return;
+        }
 
         if (count > 100) {
             return;
